17766e89d0c80f0317d490306541c0db51d6417a
Dimas Adrian Mukti <dadrianm25@gmail.com>
2022-03-28 20:26:50 +0700

    march first commit

---
+++ [2022-03] Fantom (FTM) Historical Prices Analysis/data/scraper.py
@@ -0,0 +1,24 @@
+import requests, warnings 
+import pandas as pd
+from datetime import datetime, timezone, timedelta
+warnings.filterwarnings('ignore')
+
+## Scraper fucntion
+def scraper():
+    ## Define time_start
+    time_start = datetime.utcnow().date() - timedelta(days=365)
+    time_start_ts = datetime.strptime(f"{time_start} 00:00", "%Y-%m-%d %H:%M").replace(tzinfo=timezone.utc).timestamp()
+    ## Define time_end
+    time_end = datetime.utcnow().date()
+    time_end_ts = datetime.strptime(f"{time_end} 00:00", "%Y-%m-%d %H:%M").replace(tzinfo=timezone.utc).timestamp()
+    ## Define data endpoint 
+    url = f"https://api.coinmarketcap.com/data-api/v3/cryptocurrency/historical?id=3513&convertId=2781&timeStart={int(time_start_ts)}&timeEnd={int(time_end_ts)}"
+    ## GET json return from endpoint and define dataframe
+    r = requests.get(url).json()
+    get_value = r.get('data')['quotes']
+    df = pd.DataFrame()
+    ## Append every rows from parsed return
+    for x in get_value:
+        df = df.append(x['quote'], ignore_index=True)
+    ## Write csv files based on data
+    df.to_csv('./data/{}_FTM.csv'.format(str(datetime.utcnow().date())), index=False)
